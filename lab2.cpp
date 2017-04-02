b612c8a21b3028e0ccaec9c97eb94456098709ec
Denis-12 <denis_matveev_99@mail.ru>
2017-04-03 01:36:48 +0700

    vvod

--- lab2.cpp
+++ lab2.cpp
@@ -4,4 +4,17 @@
 using namespace std;
 
 int main(){
+	int v, s;
+	cout<<"vvedite summu vklada ne <10000"<<endl;
+	cin>>v;
+	while(v<10000){
+	cout<<"vvedite summu vklada ne <10000"<<endl;
+	cin>>v;
+}
+	cout<<"vvedite srok vklada"<<endl;
+	cin>>s;
+	while(s<0 || s>365){
+		cout<<"srok vklada ne bolee 365"<<endl;
+	cin>>s;
+	}
 }
