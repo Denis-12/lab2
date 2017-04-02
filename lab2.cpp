96c8ea7abbc3047f65525e5c04985a0ea5849128
Denis-12 <denis_matveev_99@mail.ru>
2017-04-03 01:56:48 +0700

    vklad <100000tr

--- lab2.cpp
+++ lab2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-	int v, s;
+	float v, s;
 	cout<<"vvedite summu vklada ne <10000"<<endl;
 	cin>>v;
 	while(v<10000){
@@ -17,4 +17,12 @@ int main(){
 		cout<<"srok vklada ne bolee 365"<<endl;
 	cin>>s;
 	}
-}
+	
+	if(v<100000){
+		if(s<=30 )v=v-v*0.1, cout<<"summa s % ="<<v;
+		if(s>30 && s<=120) v=v+v*0.02, cout<<"summa s % ="<<v;
+		if(s>120 && s<=240) v=v+v*0.06, cout<<"summa s % ="<<v;
+		if(s>240 && s<=360) v=v+v*0.12, cout<<"summa s % ="<<v;
+	}
+}	
+	
